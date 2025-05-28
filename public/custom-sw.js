1e132529eb9795bc7c02fb4a0bb086fed01cd56a
waready <elbarto119@hotmail.com>
2025-05-27 21:48:07 -0500

    deploy

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,37 +1,29 @@
 const CACHE_NAME = 'michi-cache-v1'
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json',
-  '/js/app.js',
-  '/css/app.css',
-  '/img/icons/icon-192x192.png'
+  '/Michi-Care/',
+  '/Michi-Care/index.html',
+  '/Michi-Care/favicon.ico',
+  '/Michi-Care/manifest.json',
+  '/Michi-Care/js/app.js',
+  '/Michi-Care/css/app.css',
+  '/Michi-Care/img/icons/icon-192x192.png'
 ]
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache)
+    }).catch(err => {
+      console.error('❌ Error al agregar al caché:', err)
     })
   )
 })
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request)
-    })
-  )
-})
-
-self.addEventListener('push', function (event) {
-  const data = event.data.json()
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/img/icons/icon-192x192.png'
     })
   )
 })
@@ -42,14 +34,14 @@ self.addEventListener('push', event => {
   const title = data.title || 'Notificación'
   const options = {
     body: data.body || '',
-    icon: '/img/icons/android-chrome-192x192.png'
+    icon: '/Michi-Care/img/icons/icon-192x192.png'
   }
 
   event.waitUntil(
     self.registration.showNotification(title, options)
   )
 
-  // También envía a ventana abierta
+  // También enviar mensaje a la app si está abierta
   self.clients.matchAll({ includeUncontrolled: true }).then(clients => {
     clients.forEach(client => {
       client.postMessage(data)
